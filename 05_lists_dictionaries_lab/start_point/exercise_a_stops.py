d124dcfcadf80104665116745dbb26bf915b2c26
Becky <bjentwistle@gmail.com>
2023-04-04 16:54:02 +0100

    exercise a finished

--- 05_lists_dictionaries_lab/start_point/exercise_a_stops.py
+++ 05_lists_dictionaries_lab/start_point/exercise_a_stops.py
@@ -2,14 +2,40 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #1. Add "Edinburgh Waverley" to the end of the list
 stops.append("Edinburgh Waverley")
-print(stops)
+#print(stops)
 
 #2. Add "Glasgow Queen St" to the start of the list
+stops.insert(0, "Glasgow Queen Street")
+#print(stops)
+
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+#print(stops)
+
 #4. Print out the index position of "Linlithgow"
+print("Linlithgow is at index:", stops.index("Linlithgow"))
+#print(stops)
+
 #5. Remove "Livingston" from the list using its name
+stops.remove("Livingston")
+#print(stops)
+
 #6. Delete "Cumbernauld" from the list by index
+stops.pop(2)
+#print(stops)
+
 #7. Print the number of stops there are in the list
+print("Number of stops = " ,len(stops))
+
 #8. Sort the list alphabetically
+stops.sort()
+#print(stops)
+
 #9. Reverse the positions of the stops in the list
+stops.sort(reverse=True)
+#print(stops)
+
 #10 Print out all the stops using a for loop
+for station in stops:
+    print(station)
+
